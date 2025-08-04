443793d4c3c79c0d1b8c30e2da3c1b03c5ae1106
Aliyan Ali <darkpikayt1@gmail.com>
2025-08-04 20:00:39 +0500

    index js

---
+++ Backend/index.js
@@ -0,0 +1,30 @@
+// imorts
+import express from "express";
+import { PORT } from "./config/config.js";
+import { connectDb } from "./config/db.config.js";
+import cookieParser from "cookie-parser";
+
+ 
+//local imports
+import userRoute from "./Routes/user.Route.js";
+import videoRoute from "./Routes/video.Route.js";
+import commentRoute from "./Routes/comment.Route.js";
+
+const app=express();
+
+//middlewares
+app.use(express.json())
+app.use(cookieParser())
+
+//Routes
+app.get("/",(req,res)=>{
+    res.send("hey from server")
+});
+app.use('/api/user',userRoute);
+app.use('/api/videos', videoRoute);
+app.use('/api/comments', commentRoute)
+
+app.listen(PORT , async()=>{
+    console.log(`Server is running on port http://localhost:${PORT}`);
+    await connectDb()
+})
