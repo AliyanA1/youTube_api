8fdc3be2958a6dc5ea34c75e557e0ccc332731d9
Aliyan Ali <darkpikayt1@gmail.com>
2025-08-04 20:01:01 +0500

    config js

---
+++ Backend/config/config.js
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+
+config({path: `.env${process.env.NODE_ENV || '.development'}.local`});
+
+
+export const {
+    NODE_ENV,
+    PORT,
+    MONGO_URL ,
+    JWT_SECRET
+}= process.env;
