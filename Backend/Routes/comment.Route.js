c7a44a1661c205d54142fb78d42b1409a5031c02
Aliyan Ali <darkpikayt1@gmail.com>
2025-08-19 09:34:20 +0500

    comment route

--- Backend/Routes/comment.Route.js
+++ Backend/Routes/comment.Route.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { userMiddleware } from '../middlewares/user.middleware.js';
+import { postComment } from '../Controllers/comment.controller.js';
 
 
 const commentRoute=express.Router();
+commentRoute.use(userMiddleware);
 
-commentRoute.post("/post-comment",(req,res)=> res.send('post a comment'));
+commentRoute.post("/post-comment/:videoId",postComment);
 
 commentRoute.post("/get-comment",(req,res)=> res.send('post all comments on the spcific video'));
 
